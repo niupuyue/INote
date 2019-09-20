fb7bb8cc16f70fd04a1d950370c21f79057ae824
paulniu <niupuyue@aliyun.com>
2019-09-20 19:39:47 +0800

    创建首页java文件和布局文件

---
+++ app/src/main/java/com/paulniu/inote/TextActvity.java
@@ -0,0 +1,13 @@
+package com.paulniu.inote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Coder: niupuyue
+ * Date: 2019/9/20
+ * Time: 19:38
+ * Desc: 测试Activity
+ * Version:v0.0.1
+ */
+public class TextActvity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/paulniu/inote/ui/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.paulniu.inote.ui;
+
+import com.niupuyue.mylibrary.base.BaseActivity;
+import com.paulniu.inote.R;
+
+/**
+ * Coder: niupuyue
+ * Date: 2019/9/20
+ * Time: 19:37
+ * Desc: 首页布局
+ * Version: v0.0.2
+ */
+public class HomeActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    public void initViewById() {
+
+    }
+}
